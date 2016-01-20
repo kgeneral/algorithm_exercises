369731b99987828acd01567c5174aa4e9eed7aa8
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-01-20 13:43:01 +0900

    add test code

--- src/test/java/com/dykim/machinelearning/LinearRegressionTest.java
+++ src/test/java/com/dykim/machinelearning/LinearRegressionTest.java
@@ -2,13 +2,22 @@ package com.dykim.machinelearning;
 
 import org.junit.Test;
 
+import Jama.Matrix;
+
 public class LinearRegressionTest {
 
 	@Test
 	public void linearExpressionUsingMatrix() {
 		// Given
+		int n = 3;
+		Matrix x = new Matrix(n + 1, 1);
+		Matrix theta = new Matrix(n + 1, 1);
+
 		// When
+		Matrix hTheta = theta.transpose().times(x);
+
 		// Then
+		// System.out.println(hTheta);
 	}
 
 	@Test
